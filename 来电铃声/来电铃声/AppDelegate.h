55bffa22ecca6f2d8ffa7227e209be54f5f7eb6f
山谷里没有枪声 <1007793974@qq.com>
2017-09-17 11:58:48 +0800

    视频采集渲染

---
+++ 来电铃声/来电铃声/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  来电铃声
+//
+//  Created by 乔乐 on 2017/9/12.
+//  Copyright © 2017年 乔乐. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 来电铃声/来电铃声/voiceButton.h
@@ -0,0 +1,13 @@
+//
+//  voiceButton.h
+//  来电铃声
+//
+//  Created by 乔乐 on 2017/9/13.
+//  Copyright © 2017年 乔乐. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface voiceButton : UIButton
+
+@end
